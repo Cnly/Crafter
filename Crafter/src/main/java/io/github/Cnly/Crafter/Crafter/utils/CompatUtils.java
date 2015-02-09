089697de2988ecdd822be69fd7676c91855a1817
Cnly <minecnly@gmail.com>
2015-02-09 14:27:19 +0800

    Add a getOnlinePlayers() that support both old and new ones

---
+++ Crafter/src/main/java/io/github/Cnly/Crafter/Crafter/utils/CompatUtils.java
@@ -0,0 +1,49 @@
+package io.github.Cnly.Crafter.Crafter.utils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class CompatUtils
+{
+    
+    @SuppressWarnings("unchecked")
+    public static Collection<Player> getOnlinePlayers()
+    {
+        
+        Method m;
+        Object obj = null;
+        try
+        {
+            m = Bukkit.class.getMethod("getOnlinePlayers");
+            obj = m.invoke(null, (Object[])null);
+        }
+        catch (NoSuchMethodException | SecurityException
+                | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException e)
+        {
+            throw new RuntimeException(
+                    "Cannot invoke Bukkit.getOnlinePlayers()", e);
+        }
+        
+        if (obj instanceof Player[])
+        {
+            return Arrays.asList((Player[])obj);
+        }
+        else if (obj instanceof Collection)
+        {
+            return (Collection<Player>)obj;
+        }
+        else
+        {
+            throw new RuntimeException(
+                    "Unknown getOnlinePlayers() return type!");
+        }
+        
+    }
+    
+}
