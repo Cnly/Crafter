374d610ffb9040796688ebed70d1f356b952a981
Cnly <minecnly@gmail.com>
2015-02-19 21:51:20 +0800

    Add some methods to ItemUtils

--- Crafter/src/main/java/io/github/Cnly/Crafter/Crafter/utils/ItemUtils.java
+++ Crafter/src/main/java/io/github/Cnly/Crafter/Crafter/utils/ItemUtils.java
@@ -1,6 +1,11 @@
 package io.github.Cnly.Crafter.Crafter.utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemUtils
 {
@@ -34,10 +39,65 @@ public class ItemUtils
         String result = String.valueOf(is.getTypeId());
         short durability = is.getDurability();
         
-        if(0 != durability)
+        if (0 != durability)
             result += ":" + String.valueOf(durability);
         
         return result;
+    }
+    
+    public static List<String> getLores(ItemStack item)
+    {
+        
+        if (!item.hasItemMeta())
+            return Collections.emptyList();
+        
+        ItemMeta meta = item.getItemMeta();
+        
+        if (!meta.hasLore())
+            return Collections.emptyList();
+        
+        return meta.getLore();
+    }
+    
+    public static void addLore(ItemStack item, String lore)
+    {
+        
+        ItemMeta meta = item.getItemMeta();
+        List<String> lores = meta.hasLore() ? meta.getLore()
+                : new ArrayList<String>();
+        lores.add(lore);
+        meta.setLore(lores);
+        item.setItemMeta(meta);
+        
+    }
+    
+    public static void setLores(ItemStack item, List<String> lores)
+    {
+        
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(lores);
+        item.setItemMeta(meta);
+        
+    }
+    
+    public static String getDisplayName(ItemStack item)
+    {
+        
+        ItemMeta meta = item.getItemMeta();
+        
+        if (!meta.hasDisplayName())
+            return null;
+        
+        return meta.getDisplayName();
+    }
+    
+    public static void setDisplayName(ItemStack item, String displayName)
+    {
+        
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(displayName);
+        item.setItemMeta(meta);
+        
     }
     
 }
