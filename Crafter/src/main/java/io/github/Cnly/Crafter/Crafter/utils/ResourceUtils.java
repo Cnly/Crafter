659260daabef343cf639a973dad662a428afb86e
Cnly <minecnly@gmail.com>
2015-03-07 11:59:08 +0800

    Add more details to the exception thrown by ResourceUtils

--- Crafter/src/main/java/io/github/Cnly/Crafter/Crafter/utils/ResourceUtils.java
+++ Crafter/src/main/java/io/github/Cnly/Crafter/Crafter/utils/ResourceUtils.java
@@ -13,12 +13,18 @@ public class ResourceUtils
         throw new AssertionError("This is a util class");
     }
     
-    public static void copyFromJar(JavaPlugin jp, String location, File dest)
-            throws IOException
+    public static void copyFromJar(JavaPlugin jp, String location, File dest) throws IOException
     {
-        IOUtils.copyFileFromStream(
-                jp.getResource(location.startsWith("/") ? location.substring(1)
-                        : location), dest);
+        try
+        {
+            IOUtils.copyFileFromStream(
+                    jp.getResource(location.startsWith("/") ? location.substring(1)
+                            : location), dest);
+        }
+        catch(IOException e)
+        {
+            throw new IOException(String.format("Unable to copy %s from jar to %s!", location, dest.getAbsolutePath()), e);
+        }
     }
     
 }
